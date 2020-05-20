9eb3586e99f0f8ffecfdc2dc67524af3ce5f806a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-20 11:59:52 -0700

    rename rock-paper-scissors function

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,9 +4,13 @@ import sys
 from itertools import product
 
 def rock_paper_scissors(n):
+  # return rock_paper_scissors_unoptimized(n):
+  return rock_paper_scissors_optimized(n)
+
+def rock_paper_scissors_optimized(n):
   
   options = ["rock", "paper", "scissors"]
-  
+
   return [list(sequence_of_moves) for sequence_of_moves in product(options, repeat=n)]
 
 
@@ -16,3 +20,6 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+
+print(rock_paper_scissors(4))
