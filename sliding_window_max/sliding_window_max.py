cd9ab7f291d1ea10b4b6edb65e95f01f74c782e7
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-20 14:42:43 -0700

    add variable for third largest index

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,9 +4,10 @@ Returns: a List of integers
 '''
 
 def sliding_window_max(nums, k):
-    return sliding_window_max_unoptimizied(nums, k)
+    # return sliding_window_max_unoptimized(nums, k)
+    return sliding_window_max_optimized(nums, k)
 
-def sliding_window_max_unoptimizied(nums, k):
+def sliding_window_max_unoptimized(nums, k):
 
     # hold maxes for each window.
     total_maxes = len(nums) - k + 1
@@ -32,25 +33,51 @@ def sliding_window_max_optimized(nums, k):
     max_index = 0
 
     # also store the second largest value and its index
-    # if the max value is shifted off to the left, the second largest value will become the max
-    second_largest = None
+    # if the max value is shifted off to the left, the new max will be either...
+    # ...the second largest value in the existing window
+    # ...the incoming value
+    second_largest_value = None
     second_largest_index = None
+
+    # the second largest value always needs to be known so it can potentially be upgraded to the max value
+    # if the second largest value is shifted off to the left or if it is upgraded to max, the new second largest value will be either...
+    # ...the third largest value in the existing window
+    # ...the incoming value
+    third_largest_value = None
+    third_largest_index = None
+
+    # all values would have to be known in this case...
 
     # find max for each sliding window
     for i in range(len(nums)):
+
+        print(i)
 
         # if i < k, then the first sliding window hasn't started yet
         # take this time to find the max and second largest for the first sliding window (first k numbers)
         if i < k:
 
+            # check if a larger value is coming in
             if nums[i] > max_value:
+
+                # new value will be the max; old max will be the second largest
+                second_largest_value = max_value
+                second_largest_index = max_index
+
                 max_value = nums[i]
                 max_index = i
+
+            # check if a larger second-largest is coming in. Update it if it exists
+            elif second_largest_value and (nums[i] > second_largest_value):
+
+                second_largest_value = nums[i]
+                second_largest_index = i
 
         # each time the window shifts over to the right, determine if the max needs to be updated
         # also determine if second_largest needs to be updated
         else:
             pass
+
 
     return maxes
 
@@ -60,3 +87,9 @@ if __name__ == '__main__':
     k = 3
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+
+
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+print(sliding_window_max(arr, k))
