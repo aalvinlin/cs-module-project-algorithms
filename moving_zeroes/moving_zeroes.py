325eb466c2884313593cfefb78f31eeab69f6a7e
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-19 23:52:54 -0700

    implement 'moving zeroes'

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,12 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    
+    # return just the nonzero values in the array
+    nonzero_integers = [number for number in arr if number is not 0]
 
-    pass
+    # pad with zeroes on the right
+    return nonzero_integers + [0] * (len(arr) - len(nonzero_integers))
 
 
 if __name__ == '__main__':
