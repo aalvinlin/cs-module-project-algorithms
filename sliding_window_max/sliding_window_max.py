33941a6fa635a4c7010bb0af3e0a8e23b7d79ac6
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-20 00:39:06 -0700

    begin implementing sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,10 +3,36 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+    # hold maxes for each window
+    maxes = []
 
+    # store the current max value and its index    
+    max_value = nums[0]
+    max_index = 0
+
+    # also store the second largest value and its index
+    # if the max value is shifted off to the left, the second largest value will become the max
+    second_largest = None
+    second_largest_index = None
+
+    # find max for each sliding window
+    for i in range(len(nums)):
+
+        # if i < k, then the first sliding window hasn't started yet
+        # take this time to find the max and second largest for the first sliding window (first k numbers)
+        if i < k:
+
+            if nums[i] > max_value:
+                max_value = nums[i]
+                max_index = i
+
+        # each time the window shifts over to the right, determine if the max needs to be updated
+        # also determine if second_largest needs to be updated
+        else:
+            pass
+
+    return maxes
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
