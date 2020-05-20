0492c7f4f16d938cc151d1818ba447144a77b4a4
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-19 23:49:28 -0700

    implement 'single number'

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,13 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # sort the array
+    arr.sort()
 
-    pass
+    # look for a skip in pairs
+    for i in range(0, len(arr), 2):
+        if arr[i] is not arr[i + 1]:
+            return arr[i]
 
 
 if __name__ == '__main__':
