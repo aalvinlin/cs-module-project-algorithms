1fe2a03ce9f97011144b38fb5b4d17d8bc7ec303
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-20 17:47:22 -0700

    implement first draft of unoptimized making_change

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,11 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    
+    return 2 ** (n-1) if n > 0 else 1
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,10 +3,41 @@
 import sys
 
 def making_change(amount, denominations):
-  # Your code here
+  
+  return making_change_unoptimized(amount, denominations)
+  # return making_change_optimized(amount, denominations)
 
-  pass
+def making_change_unoptimized(amount, denominations):
 
+  print("checking for ways to make change for", amount, "with", denominations)
+
+  # there is one way to make change for 0 cents
+  # if this state is reached from a recursion call, then that means a solution was reached
+  if amount == 0:
+    print("  found a way")
+    return 1
+
+  # if there is a negative number, then too much was subtracted from the previous step. Return 0
+  elif amount < 0:
+    print("  negative amount. not possible")
+    return 0
+
+  # no coins left but there is a positive amount of cents needed: return 0 ways
+  elif len(denominations) == 0:
+    print("  no coins left to make the remaining", amount)
+    return 0
+
+  # total ways to make change:
+  #   ways to make change with the largest coin used PLUS
+  #   ways to make change without the largest coin used
+  else:
+
+    largest_coin_value = denominations[-1]
+
+    print("  will need to check more with and without", largest_coin_value)
+
+    return making_change_unoptimized(amount - largest_coin_value, denominations) + making_change_unoptimized(amount, denominations[:len(denominations) - 1]) 
+  
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
