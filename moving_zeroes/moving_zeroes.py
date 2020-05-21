a58745a11b7c9d03c371eef3f8ea752b253681a8
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-21 12:29:49 -0700

    begin optimized solution for moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,6 +3,27 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
+
+    return moving_zeroes_unoptimized(arr)
+    return moving_zeroes_optimized(arr)
+
+# optimized solution: single pass, O(1) storage
+def moving_zeroes_optimized(arr):
+
+    # use pointers to hold indices to next zero and next nonzero value
+    index_of_next_zero = None
+    index_of_next_nonzero_value = None
+
+    for i in range(len(arr)):
+
+        if arr[i] == 0:
+            index_of_next_zero = i
+        else:
+            
+
+
+
+def moving_zeroes_unoptimized(arr):
     
     # return just the nonzero values in the array
     nonzero_integers = [number for number in arr if number is not 0]
