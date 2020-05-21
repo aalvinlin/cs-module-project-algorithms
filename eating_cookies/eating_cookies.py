42da54d85b0514d596b708cd29527486b47d2012
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-21 09:14:52 -0700

    implement inefficient solution to eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,9 +2,36 @@
 Input: an integer
 Returns: an integer
 '''
+
 def eating_cookies(n):
+
+    return eating_cookies_unoptimized(n)
+    # return eating_cookies_optimized(n)
+
+
+def eating_cookies_unoptimized(n):
     
-    return 2 ** (n-1) if n > 0 else 1
+    # negative cookies left: not possible. Return 0 as a solution.
+    if n < 0:
+        return 0
+
+    # exactly 0 cookies left: 1 solution found
+    # exactly 1 cookie left: 1 way to eat
+    elif n == 0 or n == 1:
+        return 1
+    
+    # the number of ways to eat n cookies (at most 3 at a time) is equal to...
+
+    # the number ways to first eat 3 cookies at once + the number of ways to eat the remaining cookies
+    ways_if_3_cookies_eaten_first = eating_cookies_unoptimized(n - 3)
+
+    # the number ways to first eat 2 cookies at once + the number of ways to eat the remaining cookies
+    ways_if_2_cookies_eaten_first = eating_cookies_unoptimized(n - 2)
+
+    # the number ways to first eat a single cookie + the number of ways to eat the remaining cookies
+    ways_if_1_cookie_eaten_first = eating_cookies_unoptimized(n - 1)
+
+    return ways_if_3_cookies_eaten_first + ways_if_2_cookies_eaten_first + ways_if_1_cookie_eaten_first
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
